31609b7c431263f22a11642dde91307174f92606
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-06-08 12:09:36 +0100

    ✨feat: get link, title and upvote from all articles

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ response = requests.get("https://news.ycombinator.com/news") # get the response
 yc_webpage = response.text # get the text
 
 soup = BeautifulSoup(yc_webpage, "html.parser") # parse the text
-spans = soup.find_all("span", class_="titleline") # find all the spans with the class titleline
+articles = soup.find_all("span", class_="titleline") # find all the articles
 
-links = [span.find("a") for span in spans] # find all the links in the spans
+a_tags = [article.find("a") for article in articles] # find the "a" tag in the articles
 
-for link in links: # for each link
-    title_article = link.get_text() # get the text of the article
-    upvote_article = link.find_next("span", class_="score").get_text() # get the text of the upvote
-    print(upvote_article)
+for a_tag in a_tags: # for each article
+    link_article = a_tag.get("href") # get the link of the article
+    text_article = a_tag.get_text() # get the title of the article
+    upvote_article = a_tag.find_next("span", class_="score").get_text() # get the upvote
 
